e0ad085f586468b391a26ff379dff345194c38ac
Carsten Bach <mail@carsten-bach.de>
2026-05-31 16:51:17 +0200

    BUILD

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-hooks', 'wp-i18n'), 'version' => '9bcdc32e55fc87e6674a');
+<?php return array('dependencies' => array('wp-hooks', 'wp-i18n'), 'version' => '81c59414314e40c09ffa');
