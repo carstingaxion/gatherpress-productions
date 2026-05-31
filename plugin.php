0476464aee6eb460c9e4410280670ea1a00e43bf
Carsten Bach <mail@carsten-bach.de>
2026-05-31 16:41:59 +0200

    Update de translation

--- plugin.php
+++ plugin.php
@@ -11,6 +11,7 @@
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       gatherpress-productions
+ * Domain Path:       /languages
  *
  * @package GatherPress_Productions
  */
