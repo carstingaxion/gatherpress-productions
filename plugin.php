6fb28fefce181dc2f8c6b2ff5ad168d15d1bd9f2
carstingaxion <carstingaxion@users.noreply.github.com>
2026-05-31 14:47:24 +0000

    Update version-numbers to 0.2.0

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       GatherPress Productions
  * Plugin URI:        https://github.com/carstingaxion/gatherpress-productions
  * Description:
- * Version:           0.1.0
+ * Version:           0.2.0
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Requires plugins:  gatherpress
