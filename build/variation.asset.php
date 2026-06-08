a8736532bfe7967cdf8a65fc6c63ee23e8c4c999
Carsten Bach <mail@carsten-bach.de>
2026-06-08 17:13:08 +0200

    Build 2 sep files from /src

---
+++ build/variation.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '65029e6cd9742ad304c3');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /**
  * WordPress dependencies.
  */
-import { registerBlockVariation } from '@wordpress/blocks';
 import { addFilter } from '@wordpress/hooks';
 import { __ } from '@wordpress/i18n';
 
@@ -14,38 +13,3 @@ addFilter(
 			? __( 'Premiere', 'gatherpress-productions' )
 			: title
 );
-
-
-
-/**
- * Extend 'gatherpress/venue' to provide production context.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-variations/
- */
-registerBlockVariation( 'gatherpress/venue', {
-	name: 'gatherpress-productions/details',
-	title: __( 'Production Details', 'gatherpress' ),
-	description: __( 'Provides production context.', 'gatherpress' ),
-	category: 'gatherpress',
-	isActive: [ 'sourcePostType' ],
-	attributes: {
-		sourcePostType: 'gatherpress_play',
-	},
-	innerBlocks: [
-		[
-			'core/post-title',
-			{
-				level: 3,
-				isLink: true
-			}
-		],
-		[
-			'core/post-featured-image',
-			{
-				isLink: true
-			}
-		],
-	],
-	scope: [ 'inserter', 'block' ], // Defaults to 'block' and 'inserter'.
-	example: {} // Disabled like the original 'core/post-terms' block.
-} );
