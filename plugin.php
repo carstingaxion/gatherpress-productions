b60f503e87d59cdad3a4e7c88fad60410bf9946b
Carsten Bach <mail@carsten-bach.de>
2026-06-17 12:29:44 +0200

    Use the registered post type singular label as the block variation name

--- plugin.php
+++ plugin.php
@@ -60,3 +60,19 @@ function gatherpress_productions_setup(): void {
 	}
 }
 add_action( 'plugins_loaded', 'gatherpress_productions_setup' );
+
+// /**
+//  * Filters the labels of a specific post type.
+//  *
+//  * @param object $labels Object with labels for the post type as member variables.
+//  * @return object Object with labels for the post type as member variables.
+//  */
+// add_filter('post_type_labels_gatherpress_play',function ( object $labels ) : object {
+// 	$labels->name = "Stuecke";
+// 	$labels->singular_name = "Stueck";
+
+// 	return $labels;
+// } );
+
+
+
